2926642b5fc87b08bb01579829addf1f72670311
Affectieclub <95753104+Affectieclub@users.noreply.github.com>
2021-12-08 14:41:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let page = 0;
-let pageLimit = document.querySelectorAll('.page').length;
+let pageLimit = 0;
+window.onload = () => {
+    pageLimit = document.querySelectorAll('.page').length;
+    console.log(pageLimit);
 
+}
 function prevPage() {
     if(--page < 0) page = 0;
     showCurrentPage();
@@ -12,5 +16,5 @@ function nextPage() {
 }
 
 function showCurrentPage() {
-    document.querySelector('main').style.setProperty('transform', `translateX(${page * 100}%)`);
+    document.querySelector('main').style.setProperty('transform', `translateX(-${page * 100}%)`);
 }
